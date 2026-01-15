45ba7629685902e3143825aa9fc7a0dc22b3faa5
risou <153372819+ris0u@users.noreply.github.com>
2026-01-15 14:56:39 +0800

    v1.0

--- src/components/medhistory.vue
+++ src/components/medhistory.vue
@@ -1,6 +1,25 @@
-<template></template>
+<template>
+    <div class="history-wrapper">
+        <h2 class="header">Medication History</h2>
+        <p class="header-description">Track and view your medication history in one place</p>
+
+        <div class="main-content">
+            <div class="calendar-container">
+                <span class=""calendar-header>Calendar</span>
+                
+            </div>
+
+            <div class="result-history-container">
+                <span class="calendar-header"></span>
+            </div>
+        </div>
+    </div>
+</template>
 
 <script setup>
+import { ref } from 'vue'
+
+
 
 </script>
 
